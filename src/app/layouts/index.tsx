6d34f6b39a4430590fabb7f122391ca4e5e41825
KhromenkoDaniel <daniilkhro.work@gmail.com>
2025-05-12 21:59:50 +0300

    feat: initialize Next.js 14 project with App Router and FSD structure

---
+++ src/app/layouts/index.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "@/app/styles/globals.css";
+import { Inter } from 'next/font/google'
+
+export const metadata: Metadata = {
+  title: "Who wants to be a millionaire?",
+  description: "A fun quiz game where you can test your knowledge and win prizes!",
+};
+
+export const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+    weight: ["400", "600"],
+    display: "swap",
+    fallback: ["system-ui", "sans-serif"],
+});
+
+export function RootLayout({children}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <html lang="en">
+      <body className={`${inter.variable}`}>
+      {children}
+      </body>
+      </html>
+  );
+}
