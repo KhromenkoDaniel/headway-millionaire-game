e4494337cd90467bfc56e85038c497d9573c2d0f
KhromenkoDaniel <daniilkhro.work@gmail.com>
2025-05-14 19:27:47 +0300

    refactor: rename addToSum action to setSum for clarity and update related references

--- src/entities/quiz/model/actions.ts
+++ src/entities/quiz/model/actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Question } from '@entities/quiz/model/types';
 
-export const addToSum = createAction<number>('quiz/addToSum');
+export const setSum = createAction<number>('quiz/setSum');
 export const fail = createAction('quiz/fail');
 export const win = createAction('quiz/win');
 export const nextQuestion = createAction<Question>('quiz/nextQuestion');
